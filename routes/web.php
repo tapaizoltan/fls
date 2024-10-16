23506981652ff16c6a7d841add8a6dfa7a9015cc
Tapai Zoltan <zoltan@tapai.hu>
2024-10-16 15:44:19 +0200

    update: check vatNumber or Name or regNumber with e-cegkivonat.hu

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,9 @@ Route::get('/', function () {
 
 Route::get('/test', function () {
     /*
-    // nav lekérdezés
-
+    //
+    // Vállalkozás adatainak lekérdezése adószám alapján a NAV api 3.0 segítségével.
+    //
     $userData = array(
         "login" => "iwooyht42vzz9mv",
         "password" => "Zc3SGm35z9kRAX5e",
@@ -70,8 +71,10 @@ Route::get('/test', function () {
     //echo "<PRE>$text</PRE>";
     */
 
-    /*
-    $url = 'https://e-cegkivonat.hu/talalat?adoszam=Rosenberger';
+    //
+    // Vállalkozás cégjegyzékszámának lekérdezése adószám alapján az e-cégkivonat rendszeréből.
+    //
+    $url = 'https://e-cegkivonat.hu/talalat?adoszam=13638722';
 
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -82,6 +85,7 @@ Route::get('/test', function () {
         $dom = new DOMDocument();
 
         libxml_use_internal_errors(true);
+        $response = mb_convert_encoding($response, 'HTML-ENTITIES', 'UTF-8');
         $dom->loadHTML($response);
         libxml_clear_errors();
 
@@ -107,131 +111,70 @@ Route::get('/test', function () {
     } else {
         echo "Hiba a lekérés során.";
     }
-    */
 
+echo '<br><br>';
 
-/*
-//ez már egész jó
+    //
+    // Vállalkozás adatainak lekérdezése név alapján az e-cégkivonat rendszeréből.
+    //
+    $url = "https://e-cegkivonat.hu/talalat?adoszam=Rosenberger";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-    // Az oldal URL-je
-$url = "https://e-cegkivonat.hu/talalat?adoszam=Rosenberger";
+    $html = curl_exec($ch);
 
-// 1. cURL inicializálása
-$ch = curl_init();
-
-// 2. Állítsuk be a cURL opciókat
-curl_setopt($ch, CURLOPT_URL, $url); // Cél URL
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Az eredményt visszakapjuk, nem közvetlenül a kimenetre írjuk
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Kövessük az átirányításokat
-
-// 3. Lekérjük az oldal tartalmát
-$html = curl_exec($ch);
-
-// 4. Ellenőrizzük a cURL hibákat
-if (curl_errno($ch)) {
-    echo 'Hiba történt a cURL hívásban: ' . curl_error($ch);
-    curl_close($ch);
-    exit();
-}
-
-// 5. Zárjuk le a cURL session-t
-curl_close($ch);
-
-// 6. Hozz létre egy DOMDocument objektumot
-$dom = new DOMDocument();
-
-// 7. A libxml használata a HTML speciális karakterek miatt
-libxml_use_internal_errors(true); // Elkerüljük a HTML hibák megjelenítését
-
-// 8. Töltsd be a HTML tartalmat a DOMDocument objektumba
-$dom->loadHTML($html);
-
-// 9. Hozz létre egy DOMXPath objektumot a könnyebb kereséshez
-$xpath = new DOMXPath($dom);
-
-// 10. Keresd meg a hidden input típusú form elemeket
-$hiddenInputs = $xpath->query('//input[@type="hidden"]');
-
-// 11. Ellenőrizzük, hogy vannak-e rejtett mezők
-if ($hiddenInputs->length > 0) {
-    echo "<table border='1' cellpadding='5' cellspacing='0'>";
-    echo "<tr><th>Név</th><th>Érték</th></tr>"; // Táblázat fejléc
-
-    // 12. Iteráljunk végig a hidden input mezőkön és jelenítsük meg őket soronként
-    foreach ($hiddenInputs as $input) {
-        $name = $input->getAttribute('name');
-        $value = $input->getAttribute('value');
-
-        echo "<tr>";
-        echo "<td>" . htmlspecialchars($name) . "</td>";
-        echo "<td>" . htmlspecialchars($value) . "</td>";
-        echo "</tr>";
+    if (curl_errno($ch)) {
+        echo 'Hiba történt a cURL hívásban: ' . curl_error($ch);
+        curl_close($ch);
+        exit();
     }
-    echo "</table>";
-} else {
-    echo "Nincsenek rejtett input mezők az oldalon.";
-}
-    */
 
-
-$url = "https://e-cegkivonat.hu/talalat?adoszam=Rosenberger";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-$html = curl_exec($ch);
-
-if (curl_errno($ch)) {
-    echo 'Hiba történt a cURL hívásban: ' . curl_error($ch);
     curl_close($ch);
-    exit();
-}
+    $dom = new DOMDocument();
+    libxml_use_internal_errors(true);
+    $html = mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8');
+    $dom->loadHTML($html);
+    $xpath = new DOMXPath($dom);
+    $fields = ['Name', 'Zip', 'City', 'StreetAndNum', 'RegNumber', 'ShortTaxNumber'];
+    $forms = $xpath->query('//form');
+    $allData = [];
 
-curl_close($ch);
-$dom = new DOMDocument();
-libxml_use_internal_errors(true);
-$html = mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8');
-$dom->loadHTML($html);
-$xpath = new DOMXPath($dom);
-$fields = ['Name', 'Zip', 'City', 'StreetAndNum', 'RegNumber', 'ShortTaxNumber'];
-$forms = $xpath->query('//form');
-$allData = [];
-
-foreach ($forms as $form) {
-    $data = [];
-    $inputs = $form->getElementsByTagName('input');
-    foreach ($inputs as $input) {
-        $name = $input->getAttribute('name');
-        $value = $input->getAttribute('value');
-        if (in_array($name, $fields)) {
-            $data[$name] = $value;
+    foreach ($forms as $form) {
+        $data = [];
+        $inputs = $form->getElementsByTagName('input');
+        foreach ($inputs as $input) {
+            $name = $input->getAttribute('name');
+            $value = $input->getAttribute('value');
+            if (in_array($name, $fields)) {
+                $data[$name] = $value;
+            }
+        }
+        if (!empty($data)) {
+            $allData[] = $data;
         }
     }
-    if (!empty($data)) {
-        $allData[] = $data;
-    }
-}
 
-if (!empty($allData)) {
-    echo "<table border='1' cellpadding='5' cellspacing='0'>";
-    echo "<tr>";
-    foreach ($fields as $field) {
-        echo "<th>" . htmlspecialchars($field) . "</th>";
-    }
-    echo "</tr>";
-
-    foreach ($allData as $data) {
+    if (!empty($allData)) {
+        echo "<table border='1' cellpadding='5' cellspacing='0'>";
         echo "<tr>";
         foreach ($fields as $field) {
-            echo "<td>" . (isset($data[$field]) ? htmlspecialchars($data[$field]) : 'N/A') . "</td>";
+            echo "<th>" . htmlspecialchars($field) . "</th>";
         }
         echo "</tr>";
-    }
 
-    echo "</table>";
-} else {
-    echo "Nincsenek megfelelő találatok az oldalon.";
-}
+        foreach ($allData as $data) {
+            echo "<tr>";
+            foreach ($fields as $field) {
+                echo "<td>" . (isset($data[$field]) ? htmlspecialchars($data[$field]) : 'N/A') . "</td>";
+            }
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "Nincsenek megfelelő találatok az oldalon.";
+    }
 
 });
